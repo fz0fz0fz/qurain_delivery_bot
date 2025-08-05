708c6936b540d925407ea758933434e94763c397
fz0fz0fz <fz0fz0fz@gmail.com>
2025-08-05 14:39:09 +0300

    Update init_pg_drivers.py

--- init_pg_drivers.py
+++ init_pg_drivers.py
@@ -1,5 +1,5 @@
 import psycopg2
-from dp_utils import PG_CONN_INFO  # استيراد بيانات الاتصال من ملف dp_utils.py
+from db_utils import PG_CONN_INFO  # استيراد بيانات الاتصال من ملف db_utils.py
 
 def create_drivers_table_pg():
     conn = psycopg2.connect(**PG_CONN_INFO)
