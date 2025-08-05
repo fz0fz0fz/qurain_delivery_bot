b88681583de5908e550163771d66001f29ed2eea
fz0fz0fz <fz0fz0fz@gmail.com>
2025-08-05 13:44:30 +0300

    Create init_pg_drivers.py

---
+++ init_pg_drivers.py
@@ -0,0 +1,15 @@
+def create_drivers_table_pg():
+    conn = psycopg2.connect(**PG_CONN_INFO)
+    cur = conn.cursor()
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS drivers (
+            id SERIAL PRIMARY KEY,
+            name TEXT NOT NULL,
+            phone TEXT UNIQUE NOT NULL,
+            user_id TEXT,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+    """)
+    conn.commit()
+    cur.close()
+    conn.close()
