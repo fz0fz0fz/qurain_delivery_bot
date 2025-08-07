420bc0a6019fc618d08e995513f66f44f521c932
fz0fz0fz <fz0fz0fz@gmail.com>
2025-08-07 07:30:32 +0300

    Update dispatcher.py

--- dispatcher.py
+++ dispatcher.py
@@ -1,3 +1,4 @@
+from driver_register import handle_driver_service 
 from pg_utils import generate_order_id_pg
 from send_utils import send_message
 from services.unified_service import handle_service
@@ -6,7 +7,6 @@ import re
 
 from search_utils import search_services_arabic
 from services_data import SERVICES
-from driver_register import handle_driver_service  # فقط هذه الدالة
 
 
 
